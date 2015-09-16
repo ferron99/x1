21e60bddc1ef5bb1def6c9355e71c8d1a4961707
ferron99 <nferro@terpmail.umd.edu>
2015-09-16 11:29:54 -0400

    Change Description
    Added tree and house, changed creature, added simple physics and gravity to creature. Spent time attempting to create a randomly generated forest, couldn't quite get it to work.

--- x1.java
+++ x1.java
@@ -1,10 +1,15 @@
-//////// What am I trying to do???
-//////// What is my name?  (CST 112; today's date?)
+
+//////// x1
+////////Nick Ferro  (CST 112; Sept 16 2015)
+/////// WIP, Incomplete, more features planned/in-progress
 
 //// GLOBALS:  coordinates, speed, etc.
 float x, y;       // Position of creature.
 float dx, dy;     // Speed.
 float horizon;
+float gravity;
+float treeX = 150;
+float treeY = 250;
 
 //// SETUP:  window size, initialization (start in middle of screen).
 void setup() {
@@ -14,6 +19,7 @@ void setup() {
   y=  height/2;
   dx=  3;
   dy=  2;
+  gravity = 0.1;
 }
 
 //// NEXT FRAME:  scene, action, show.
@@ -27,22 +33,57 @@ void draw() {
   rect( 0,horizon, width,height*3/4 );      // grass.
   
   /* INSERT YOUR CODE HERE! */
-  triangle( 150,horizon, 120,horizon-50, 180,horizon-50  );  // tree
-  text( "This is NOT a good tree; please fix it!", 150,horizon );
-                                            // house
+  noStroke();
+  fill(139,69,19);
+  rect(treeX-5, treeY-50, 10, 60);
+  fill(34,139,34);
+  quad(treeX-50, treeY-50, treeX-25, treeY-100, treeX+25, treeY-100, treeX+50, treeY-50);
+  quad(treeX-40, treeY-100, treeX-15, treeY-150, treeX+15, treeY-150, treeX+40, treeY-100);
+  triangle(treeX-30, treeY-150, treeX+30, treeY-150, treeX, treeY-210);
+  
+  fill(175);                                          // house
+  rect(400,horizon+100,150,150);
+  fill(150);
+  triangle(400,horizon+100,550,horizon+100,475,horizon+50);
+  fill(0);
+  rect(465,horizon+210,20,40);
+  fill(255,255,0);
+  rect(430,horizon+140, 20,20);
+  rect(500,horizon+140, 20,20);
+ 
 
   fill(0);
-  text( "My name is Mud", 10,height-20 );                                          
+  text( "My name is Nick", 10,height-20 );                                          
                                             
   //// ACTION:  move (x,y) coordinates.
   x=  x + dx;
   y=  y + dy;
+  dy = dy + gravity;
   
   //// SHOW:  display the creature at (x,y)
 
   /* INSERT YOUR CODE HERE! */
-  fill(255,0,0); rect( x,y, 30,50 );        /* REPLACE THIS WITH YOUR OWN CODE! */
-  text( "Fred", x,y );
+  fill(255,0,255);
+  noStroke();
+  rect(x,y,20,10);
+  rect(x,y+9,10,21);
+  rect(x+9,y+20,32,10);
+  rect(x+40,y+9,10,21);
+  rect(x+30,y,20,10);
+  rect(x+10,y+29,10,11);
+  rect(x+30,y+29,10,11);
+  rect(x+20,y+39,10,10);
+  fill(0);
+  text( "Ferron", x,y );
+  if ((x>width-50) || (x<0)){
+    dx = dx * -.95;
+  }
+ if (y < 0) {
+   dy = dy * -1;
+ }
+   if (y>height-50) {
+    dy = dy * -0.95;
+  }
 
 }
 
@@ -52,8 +93,8 @@ void mousePressed() {
   x=  mouseX;                             // Set (x,y) to mouse
   y=  mouseY;
   //
-  dx=  random( -6, +6 );                  // random speed.
-  dy=  random( -4, +4 );
+  dx=  random( -20, +20 );                  // random speed.
+  dy=  random( -14, +14 );
 }
 
 void keyPressed() {
